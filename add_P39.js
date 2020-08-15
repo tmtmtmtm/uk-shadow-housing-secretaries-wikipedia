317c67a122caa5937490937cc8fe3bbaa0033a6d
Tony Bowden <tony@tmtm.com>
2020-08-15 12:19:33 +0100

    Config via add_P39

--- add_P39.js
+++ add_P39.js
@@ -16,11 +16,11 @@ module.exports = (id, startdate, enddate, replaces, replacedby, ordinal, cabinet
     id,
     claims: {
       P39: {
-        value: 'Q7460598',
+        value: 'Q30645478',
         qualifiers: qualifiers,
         references: {
           P143: 'Q328', // English Wikipedia
-          P4656: 'https://en.wikipedia.org/wiki/Shadow_Secretary_of_State_for_Justice'
+          P4656: 'https://en.wikipedia.org/wiki/Shadow_Secretary_of_State_for_Housing'
         },
       }
     }
